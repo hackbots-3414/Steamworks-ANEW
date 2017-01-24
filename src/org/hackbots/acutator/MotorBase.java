37d581707498f5578eaf5da1f129fe993b5d5952
Tyler Newton <tylernewton08@gmail.com>
2017-01-23 20:02:03 -0500

    Made the motors great again

--- src/org/hackbots/acutator/MotorBase.java
+++ src/org/hackbots/acutator/MotorBase.java
@@ -1,5 +1,9 @@
 package org.hackbots.acutator;
 
-public abstract class MotorBase {
-
+public abstract class MotorBase 
+{
+	public abstract void setSpeed(double speed);
+	
+	
+	public abstract void stop();
 }
